c4d3f8b19404d054c44af3d296cc3be58fca6bf6
Pushpesh Kumar <Pushpesh.Kumar@dnb.no>
2022-01-04 13:20:06 +0100

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,6 +5,9 @@ public class LoginPage {
 	public static void main(String args[ ]) { 
 			
 			System.out.println("Hello LP");
+			String un = "admin";
+			String pwd = "admin";
+			System.out.println("logged in...");
 		}
 
 
