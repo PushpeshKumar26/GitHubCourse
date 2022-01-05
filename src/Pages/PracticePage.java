669a7725b1464c782825397526344c812cb423f5
Chena Srika <chenasrika00@gmail.com>
2022-01-05 17:03:37 +0100

    practice commit 2

--- src/Pages/PracticePage.java
+++ src/Pages/PracticePage.java
@@ -5,6 +5,8 @@ public class PracticePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Practice 1");
+		
+		System.out.println("Practice 2");
 
 	}
 
