37df7d832ea513f9a260fba698702c3eb1168134
Pushpesh Kumar <Pushpesh.Kumar@dnb.no>
2022-01-05 18:57:44 +0100

    added cart page

---
+++ src/Pages/CartPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class CartPage {
+	
+	public static void cart(){ 
+		
+	    // developer 1 Pushpesh
+		String cartName="Pushpesh";
+		System.out.println(cartName);
+		
+		
+	}
+
+}
