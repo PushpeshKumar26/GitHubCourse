b553d18e813ee845ad563df8978800ef6d6e564b
PushpeshKumar26 <55064096+PushpeshKumar26@users.noreply.github.com>
2022-01-05 18:53:03 +0100

    Create CartPage.java

---
+++ src/Pages/CartPage.java
@@ -0,0 +1,17 @@
+package Pages;
+
+public class CartPage {
+	
+	public static void cart(){ 
+		
+	    // developer 2 
+		String cartName="Chena";
+		System.out.println(cartName);
+		
+    // ham do hame do
+		String bulb="hena";
+		System.out.println(bulb);
+		
+	}
+
+}
